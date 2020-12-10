04625a26a7963d627b1f98efe6b42826833fa0cd
CrownKira <t.karwi@yahoo.com>
2020-12-10 08:16:16 +0800

    Added edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,36 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        # ActionController::Parameters
+        # render plain: params[:article].class #{"title"=>"test", "description"=>"testing"}
+        @article = Article.new(params.require(:article).permit(:title, :description)) #require top level key of article
+        # permit title and description to be used to create the object
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else 
+            render 'new' #ie. return the new template
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render "edit"
+        end
+    end
+
 end
