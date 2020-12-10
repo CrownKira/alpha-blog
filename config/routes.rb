f381b54f52e0391d9ee7a0b088a7e00eadfd82bd
CrownKira <t.karwi@yahoo.com>
2020-12-10 22:50:05 +0800

    Add user create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   # only: [:actionname, ...]
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+
+  resources :users, except: [:new]
 end
